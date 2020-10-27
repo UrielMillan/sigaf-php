513d7c754eca94e1218114b55e42b50c26789373
erick uriel <uriel.noriegam@gmail.com>
2020-10-26 22:53:54 -0700

    added new get files to brands, categories and departments

--- Connection.php
+++ Connection.php
@@ -7,7 +7,7 @@
     $db_database="sigaf";
     $db_port = '3306';
 
-    mysqli_report(MYSQLI_REPORT_ALL);
+    mysqli_report(MYSQLI_REPORT_STRICT);
 		$con = mysqli_connect($db_host,$db_user,$db_password,$db_database,$db_port);
 
     if(!mysqli_connect_errno()){
---
+++ GetDepartments.php
@@ -0,0 +1,32 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  require_once('Connection.php');
+
+  $lowerLimit = $_GET['lowerLimit'];
+  $upperLimit = $_GET['upperLimit'];
+  $response['success'] = null;
+  $response['data'] = array();
+
+  try {
+    $con = Connect();
+    $sql='SELECT id, code, description, note, status FROM departments LIMIT ?,?';
+    $stmt = $con->prepare($sql);
+    $stmt->bind_param('ii', $lowerLimit, $upperLimit);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data= array();
+    while($row = $result->fetch_assoc()){
+      array_push($data, $row);
+    }
+    $response['success'] = true;
+    $response['data'] = $data;
+  } catch (mysqli_sql_exception $e) {
+    echo "Error: " . $e->getMessage();
+    $response['success'] = false;
+    $response['data'] = null;
+  }finally{
+    mysqli_close($con);
+    echo json_encode($response);
+  }
+
+?>
